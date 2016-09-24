eaf266d8c6857b94ba51001e119d16e485c82009
Sireesha Muppala <sireesha.muppala@bhnetwork.com>
2016-09-23 18:28:00 -0600

    Support for new APIs

--- server.js
+++ server.js
@@ -7,8 +7,49 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+var todos = [
+    {
+        id: 1,
+        description: 'Meet mom for lunch',
+        completed: false
+    },
+    {
+        id: 2,
+        description: 'Pick up groceries',
+        completed: false
+    },
+    {
+        id: 3,
+        description: 'Fill Timesheet',
+        completed: true
+    }
+];
+
 app.get('/', function(req,res){
     res.send('Todo API Root')
+});
+
+app.get('/todos', function(req,res) {
+    res.json(todos);
+});
+
+app.get('/todos/:id', function(req,res) {
+
+    var todoId = req.params.id;
+    var matchToDo = undefined;
+    //res.send('Asking for todo with id of ' + todoId);
+    //res.json(todos[id]);
+    for(i = 0; i< todos.length; i++) {
+        if (todos[i].id === parseInt(todoId)) {
+            matchToDo = todos[i];
+        }
+    }
+    if(matchToDo) {
+        res.json(matchToDo)
+    } else {
+        res.status(404).send();
+    }
+
 });
 
 app.listen(port, function(){
